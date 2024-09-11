16147ea4be7053096d9410affe684979c9718c90
voenkovalexander <156917408+voenkovalexander@users.noreply.github.com>
2024-09-11 22:26:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(".cat-search>button").click(search)
+
+$(".cat-search>input").keydown(function(event){
+    if(event.key === "Enter"){
+        search()
+    }
+})
+
+
+function search (){
+    console.log("111")
+    var val = $(".cat-search>input").val()
+
+    $(".product-list>h3").each(function(){
+        let card = $(this).closest('.product-item');
+        let text = $(this).text();
+        text = text.toLowerCase()
+        text.includes(val)? card.show() : card.hide();
+    })
+}
+
+var chekMark = $(".check>div>img")
+
+$(document).ready(function(){
+    chekMark.hide()
+    $(".check>div").click(function(){
+        chekMark.toggle()
+    })
+})
